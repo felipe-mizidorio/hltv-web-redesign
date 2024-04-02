ecbe52858473ee78155736a6d8afc8baac34ccd8
Elquiasjr <elquiasj@gmail.com>
2024-04-02 15:34:38 -0300

    feat: images are cicled in a interval

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
+setInterval(() => {
+    const images = document.querySelector("[data-images]")
+    const activeImage = images.querySelector("[data-active]")
+    let newIndex = [...images.children].indexOf(activeImage) + 1
+
+    if (newIndex < 0) newIndex = images.children.length - 1
+    if (newIndex >= images.children.length) newIndex = 0
+
+    images.children[newIndex].dataset.active = true
+    delete activeImage.dataset.active
+} , 4000)
+
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const offset = button.dataset.carouselButton === "next" ? 1 : -1
