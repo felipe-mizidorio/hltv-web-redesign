123ee7e56d9e62f3b23e46a1489cc7c943b4b43d
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2024-04-04 14:11:41 -0300

    refactor: change background parameter

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
+const buttons = document.querySelectorAll("[data-carousel-button]");
 
 setInterval(() => {
-    const images = document.querySelector("[data-images]")
-    const activeImage = images.querySelector("[data-active]")
-    let newIndex = [...images.children].indexOf(activeImage) + 1
+  const images = document.querySelector("[data-images]");
+  const activeImage = images.querySelector("[data-active]");
+  let newIndex = [...images.children].indexOf(activeImage) + 1;
 
-    if (newIndex < 0) newIndex = images.children.length - 1
-    if (newIndex >= images.children.length) newIndex = 0
+  if (newIndex < 0) newIndex = images.children.length - 1;
+  if (newIndex >= images.children.length) newIndex = 0;
 
-    images.children[newIndex].dataset.active = true
-    delete activeImage.dataset.active
-} , 4000)
+  images.children[newIndex].dataset.active = true;
+  delete activeImage.dataset.active;
+}, 4000);
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const images = button
-            .closest("[data-carousel]")
-            .querySelector("[data-images]")
-        
-        const activeImage = images.querySelector("[data-active]")
-        let newIndex = [...images.children].indexOf(activeImage) + offset
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const images = button
+      .closest("[data-carousel]")
+      .querySelector("[data-images]");
 
-        if (newIndex < 0) newIndex = images.children.length - 1
-        if (newIndex >= images.children.length) newIndex = 0
+    const activeImage = images.querySelector("[data-active]");
+    let newIndex = [...images.children].indexOf(activeImage) + offset;
 
-        images.children[newIndex].dataset.active = true
-        delete activeImage.dataset.active
-    })
-})
+    if (newIndex < 0) newIndex = images.children.length - 1;
+    if (newIndex >= images.children.length) newIndex = 0;
+
+    images.children[newIndex].dataset.active = true;
+    delete activeImage.dataset.active;
+  });
+});
